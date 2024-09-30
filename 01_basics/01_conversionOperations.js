5292c7aa344b1f3443101f871cc073a05800fb9d
Rohan Khandelwal <128305630+rohan-kl@users.noreply.github.com>
2024-09-30 16:09:15 +0000

    done

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+console.log(score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN  (not a number)
+//true =>1, false => 0
+
+let isLoggedIn = 1
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//"" => false
+//"rohan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
